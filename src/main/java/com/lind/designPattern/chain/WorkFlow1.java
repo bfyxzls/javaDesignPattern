fcf3e2798d29b01fefe9868730574e42904885a2
bfyxzls <bfyxzls@sina.com>
2019-11-05 14:23:43 +0800

    完善责任链

--- src/main/java/com/lind/designPattern/chain/WorkFlow1.java
+++ src/main/java/com/lind/designPattern/chain/WorkFlow1.java
@@ -1,13 +1,16 @@
 package com.lind.designPattern.chain;
 
+/**
+ * 第一个责任链条.
+ */
 public class WorkFlow1 extends WorkFlow {
+  @Override
+  protected ChainHandler getChainHandler() {
+    ChainHandler chainHandler = new CreateService();
+    chainHandler.setNext(new EditService())
+        .setNext(new RemoveService())
+        .setNext(new ReadService());
+    return chainHandler;
+  }
 
-    @Override
-    public void initCommand() {
-        ChainHandler chainHandler = new CreateService();
-        chainHandler.setNext(new EditService())
-                .setNext(new RemoveService())
-                .setNext(new ReadService());
-        setCommand(chainHandler);
-    }
 }
--- src/main/java/com/lind/designPattern/chain/WorkFlow2.java
+++ src/main/java/com/lind/designPattern/chain/WorkFlow2.java
@@ -1,12 +1,16 @@
 package com.lind.designPattern.chain;
 
+/**
+ * 第二个责任链条.
+ */
 public class WorkFlow2 extends WorkFlow {
 
-    @Override
-    public void initCommand() {
-        ChainHandler chainHandler = new CreateService();
-        chainHandler.setNext(new ReadService())
-                .setNext(new EditService());
-        setCommand(chainHandler);
-    }
+  @Override
+  protected ChainHandler getChainHandler() {
+
+  ChainHandler chainHandler=  new CreateService();
+    chainHandler.setNext(new ReadService())
+        .setNext(new EditService());
+    return chainHandler;
+  }
 }
